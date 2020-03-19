7b16f15737ff99c4a1befc5c7020579d23b3818b
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2020-03-19 12:32:53 -0600

    ovv ticker w dygraph

--- app_sp500.R
+++ app_sp500.R
@@ -5,10 +5,10 @@ setwd("~")
 first.date <- Sys.Date()-2000
 sp <- GetSP500Stocks()
 ovv <- "OVV"
-# sp_list <- BatchGetSymbols(tickers = sp$Tickers,
-#                            first.date = first.date,
-#                            last.date = Sys.Date(),
-#                            do.cache=TRUE)
+sp_list <- BatchGetSymbols(tickers = sp$Tickers,
+                          first.date = first.date,
+                          last.date = Sys.Date(),
+                          do.cache=TRUE)
 
 ovv_list <- BatchGetSymbols(tickers = ovv,
                             first.date = first.date,
